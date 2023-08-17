e328a3f61a9da0ad4206c680d12b8133545ae33a
whiplade <nmawira13@gmail.com>
2023-08-17 07:44:29 +0300

    Completed Assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    if length == 0:
-        print([])
+    if length <= 0:
+        print ([])
+        return 
+    
     elif length == 1:
         print([0])
-    elif length == 2:
-        print([0, 1])
-    elif length == 10:
-        print([0, 1, 1, 2, 3, 5, 8, 13, 21, 34])
+        return 
+    
+    fibonacci_sequence = [0, 1]
+    while len(fibonacci_sequence) < length:
+        next_number = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_number)
+
+    print(fibonacci_sequence)
